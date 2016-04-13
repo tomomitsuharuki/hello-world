96d8d7a1ebcdf6218a0aca6b76cb447fe3ed296b
Tomomitsu Haruki <tomomitsu.haruki@gmail.com>
2016-04-13 23:58:53 +0900

    change tmp directory

--- merge/archive.sh
+++ merge/archive.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo git archive
+rm -rf tmp/*
 cd ..
-git archive --verbose --format=tar master test_app | tar -C merge/archive -xf -
+git archive --verbose --format=tar master test_app | tar -C merge/tmp -xf -
+git log > merge/tmp/log.txt
