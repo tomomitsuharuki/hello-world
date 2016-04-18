3859cd0d8d820a029e72f553fcd0da689fa70c0e
Tomomitsu Haruki <tomomitsu.haruki@gmail.com>
2016-04-18 23:17:41 +0900

    fix log command

--- log.sh
+++ log.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-
-
+git log --pretty=format:'%C(yellow)commit: %h%Creset %x0A %ci %x0A %s %x0A' master..log_test
+echo ----- change files -----
+git diff --name-status master...log_test
 
